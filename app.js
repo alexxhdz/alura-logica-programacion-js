8808e4814ce85cd8a25b27a59fc3cfaaba0f356b
alexxhdz <ale.hz.contacto@gmail.com>
2024-01-28 15:06:55 -0600

    style(css): Mejoras responsivas
    Co-Authored-By: Alejandro Hernández <118626234+alexxhdz@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// Variables en JS
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
 
+// Numero que el usuario ingrese en el prompt
+let numeroUsuario = prompt("Ingresa un numero entre 1 y 10:");
+
+// Imprime en la consola el numero que ha ingresado el usuario
 console.log(numeroUsuario);
 
+// Bloque de codigo condicional
 if (numeroUsuario == numeroSecreto) {
+    // La condicion fue verdadera
     alert('Acertaste el numero');
+} else {
+    // La condicion no se cumplio
+    alert('No acertaste el numero');
 }
