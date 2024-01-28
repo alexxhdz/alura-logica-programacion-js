c4d2ef1b4dd2582cdb1af0f397769e6008caa4ea
alexxhdz <ale.hz.contacto@gmail.com>
2024-01-28 15:36:58 -0600

    feat(js): Agregado de pistas en el juego
    Co-Authored-By: Alejandro Hernández <118626234+alexxhdz@users.noreply.github.com>

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ console.log(numeroUsuario);
 // Bloque de codigo condicional
 if (numeroUsuario == numeroSecreto) {
     // La condicion fue verdadera
-    alert('Acertaste el numero');
+    // alert('Acertaste el numero, el numero correcto era: ' + numeroSecreto);
+    alert('Acertaste el numero, el numero correcto era: ' + numeroUsuario);
 } else {
     // La condicion no se cumplio
     alert('No acertaste el numero');
+    // Agregado de un condicional anidado
+    if (numeroUsuario > numeroSecreto){
+        // Si el numero secreto es menor, imprime este mensaje
+        alert("El numero secreto es menor");
+    } else {
+        // Si el numero secreto es mayor, imprime este mensaje
+        alert("El numero secreto es mayor");
+    }
 }
