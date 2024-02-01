0b676849c3b6f3452fbf2ecc31dca445b6944f30
alexxhdz <ale.hz.contacto@gmail.com>
2024-01-31 22:27:55 -0600

    feat(): Agregado de un ciclo while
    Co-Authored-By: Alejandro Hernández <118626234+alexxhdz@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // Variables en JS
 let numeroSecreto = 6;
-
-// Numero que el usuario ingrese en el prompt
-let numeroUsuario = prompt("Ingresa un numero entre 1 y 10:");
-
 // Imprime en la consola el numero que ha ingresado el usuario
-console.log(numeroUsuario);
+// console.log(numeroUsuario);
 
-// Bloque de codigo condicional
-if (numeroUsuario == numeroSecreto) {
-    // La condicion fue verdadera
-    // alert('Acertaste el numero, el numero correcto era: ' + numeroSecreto);
-    alert('Acertaste el numero, el numero correcto era: ' + numeroUsuario);
-} else {
-    // La condicion no se cumplio
-    alert('No acertaste el numero');
-    // Agregado de un condicional anidado
-    if (numeroUsuario > numeroSecreto){
-        // Si el numero secreto es menor, imprime este mensaje
-        alert("El numero secreto es menor");
+let numeroUsuario = 0;
+
+// Mientras que numero de usuario y numero secreto no sean iguales,
+// repite este bloque de codigo
+while(numeroUsuario != numeroSecreto){
+    // Numero que el usuario ingrese en el prompt
+    let numeroUsuario = prompt("Ingresa un numero entre 1 y 10:");
+        // Bloque de codigo condicional
+    if (numeroUsuario == numeroSecreto) {
+        // La condicion fue verdadera
+        // alert('Acertaste el numero, el numero correcto era: ' + numeroSecreto);
+        alert('Acertaste el numero, el numero correcto era: ' + numeroUsuario);
     } else {
-        // Si el numero secreto es mayor, imprime este mensaje
-        alert("El numero secreto es mayor");
+        // La condicion no se cumplio
+        alert('No acertaste el numero');
+        // Agregado de un condicional anidado
+        if (numeroUsuario > numeroSecreto){
+            // Si el numero secreto es menor, imprime este mensaje
+            alert("El numero secreto es menor");
+        } else {
+            // Si el numero secreto es mayor, imprime este mensaje
+            alert("El numero secreto es mayor");
+        }
     }
 }
